8de05516fd375d7457bcc02ec88375710286f418
Subtixx <subtixx@gmail.com>
2016-08-08 14:46:00 +0200

    Removed fullbattery from game due to parameters

--- interface/scripted/basicgenerator/basicgeneratorgui.lua
+++ interface/scripted/basicgenerator/basicgeneratorgui.lua
@@ -11,7 +11,7 @@ function update(dt)
 	local energy = world.getObjectParameter( pane.containerEntityId() , "energy", 0)
 	local energyMax = world.getObjectParameter( pane.containerEntityId() , "energyMax", 0)
 
-	widget.setText("lblDynEnergy", string.format("%d/%d F", energy, energyMax))
+	widget.setText("lblDynEnergy", string.format("%d/%d F", math.floor(energy), math.floor(energyMax)))
 
 	widget.setProgress("pbDynFuel", math.min(1, fuel / fuelMax))
 	widget.setProgress("pbDynEnergy", math.min(1, energy / energyMax))
--- objects/wired/energy/batteryblock/batteryblock.lua
+++ objects/wired/energy/batteryblock/batteryblock.lua
@@ -1,68 +1,76 @@
 function init(virtual)
-  if not virtual then
-    energy.init()
+   if virtual then return end
+   energy.init()
 
-    self.particleCooldown = 0.2
-    self.particleTimer = 0
+   self.particleCooldown = 0.2
+   self.particleTimer = 0
 
-    self.acceptCharge = config.getParameter("acceptCharge") or true
+   self.acceptCharge = config.getParameter("acceptCharge") or true
 
-    animator.setParticleEmitterActive("charging", false)
-    updateAnimationState()
-  end
+   animator.setParticleEmitterActive("charging", false)
+
+   --animator.translateTransformationGroup("chargebar", {0.125, 0.375})
+
+   updateAnimationState()
 end
 
 function die()
-  local position = entity.position()
-  if energy.getEnergy() == 0 then
-    world.spawnItem("batteryblock", {position[1] + 0.5, position[2] + 1}, 1)
-  elseif energy.getUnusedCapacity() == 0 then
-    world.spawnItem("fullbattery", {position[1] + 0.5, position[2] + 1}, 1, {savedEnergy=energy.getEnergy()})
-  else
-    world.spawnItem("batteryblock", {position[1] + 0.5, position[2] + 1}, 1, {savedEnergy=energy.getEnergy()})
-  end
+   local position = entity.position()
+   --[[if energy.getEnergy() == 0 then
+      world.spawnItem("batteryblock", {position[1] + 0.5, position[2] + 1}, 1)
+   elseif energy.getUnusedCapacity() == 0 then
+      world.spawnItem("fullbattery", {position[1] + 0.5, position[2] + 1}, 1, {savedEnergy=energy.getEnergy()})
+   else
+      world.spawnItem("batteryblock", {position[1] + 0.5, position[2] + 1}, 1, {savedEnergy=energy.getEnergy()})
+   end]]
+   world.spawnItem("batteryblock", {position[1] + 0.5, position[2] + 1}, 1, {
+      savedEnergy=energy.getEnergy(),
+      description=string.format(config.getParameter("energyDescription"), math.floor(energy.getEnergy())),
+      inventoryIcon="fullbatteryicon.png" -- TODO: Figure out how to override "largeImage"
+   })
 
-  energy.die()
+   energy.die()
 end
 
 function isBattery()
-  return true
+   return true
 end
 
 function getBatteryStatus()
-  return {
-    id=entity.id(),
-    capacity=energy.getCapacity(),
-    energy=energy.getEnergy(),
-    unusedCapacity=energy.getUnusedCapacity(),
-    position=entity.position(),
-    acceptCharge=self.acceptCharge
-  }
+   return {
+      id=entity.id(),
+      capacity=energy.getCapacity(),
+      energy=energy.getEnergy(),
+      unusedCapacity=energy.getUnusedCapacity(),
+      position=entity.position(),
+      acceptCharge=self.acceptCharge
+   }
 end
 
 function onEnergyChange(newAmount)
-  updateAnimationState()
+   updateAnimationState()
 end
 
 function showChargeEffect()
-  animator.setParticleEmitterActive("charging", true)
-  self.particleTimer = self.particleCooldown
+   animator.setParticleEmitterActive("charging", true)
+   self.particleTimer = self.particleCooldown
 end
 
 function updateAnimationState()
-  local chargeAmt = energy.getEnergy() / energy.getCapacity()
-  --entity.scaleGroup("chargebar", {1, chargeAmt})
-  sb.logInfo(string.format("Energy update: %f", chargeAmt))
-  animator.scaleTransformationGroup("chargebar", {1, chargeAmt})
+   local chargeAmt = energy.getEnergy() / energy.getCapacity()
+   --entity.scaleGroup("chargebar", {1, chargeAmt})
+   animator.resetTransformationGroup("chargebar")
+   animator.scaleTransformationGroup("chargebar", {1, chargeAmt})
+   animator.translateTransformationGroup("chargebar", {0.125, 0.375})
 end
 
 function update(dt)
-  if self.particleTimer > 0 then
-    self.particleTimer = self.particleTimer - dt
-    if self.particleTimer <= 0 then
-      animator.setParticleEmitterActive("charging", false)
-    end
-  end
+   if self.particleTimer > 0 then
+      self.particleTimer = self.particleTimer - dt
+      if self.particleTimer <= 0 then
+         animator.setParticleEmitterActive("charging", false)
+      end
+   end
 
-  energy.update(dt)
+   energy.update(dt)
 end
